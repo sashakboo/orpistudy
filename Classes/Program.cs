0b6226f0480b1eaa7dd69b8e014349b424e340f7
Кильдибеков Александр (Kildibekov_AR) <Kildibekov_AR@directum.ru>
2021-08-19 12:25:52 +0400

    Доработки по работе с типами

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Classes
 {
@@ -13,6 +14,8 @@ namespace Classes
       var work = new Work();
       var noEntity = new NoEntity();
 
+      Console.WriteLine(work.GetType().IsAssignableFrom(memo.GetType()));
+
       var objs = new List<(object, object)>()
       {
         (entity, entity),
@@ -21,27 +24,32 @@ namespace Classes
         (entity, noEntity),
         (memo, extendedMemo),
         (extendedMemo, memo),
+        (extendedMemo, entity),
         (work, memo),
         (work, entity)
       };
 
       foreach (var (item1, item2) in objs)
       {
+        Console.Write($"{item1.GetType().FullName} and {item2.GetType().FullName}:\r\n\t");
+
         try
         {
           string relation = ClassRelations(item1, item2);
           Console.WriteLine(relation);
+          string commonAncestor = GetCommonAncestors(item1, item2);
+          Console.WriteLine($"\tОбщие предки: {commonAncestor}");
         }
         catch (Exception ex)
         {
-          Console.WriteLine(ex);
+          Console.WriteLine(ex.Message);
         }
       }
 
       Console.ReadKey();
     }
 
-    static string ClassRelations<T1, T2>(T1 obj1, T2 obj2)
+    static string ClassRelations(object obj1, object obj2)
     {
       if (obj1 == null)
         throw new ArgumentNullException(nameof(obj1));
@@ -58,19 +66,46 @@ namespace Classes
       if (type2.IsValueType)
         throw new ArgumentException(nameof(obj2), $"{nameof(obj2)} не является экземпляром класса");
 
-      if (type1.Equals(type2))
+      if (type1 == type2)
         return $"Оба переданных объекта являются экземплярами класса {type1.FullName}";
 
-      if (type1.BaseType.Equals(type2))
+      if (type1.IsSubclassOf(type2))
         return $"Класс {type2.FullName} является предком класса {type1.FullName}";
 
-      if (type2.BaseType.Equals(type1))
+      if (type2.IsSubclassOf(type1))
         return $"Класс {type1.FullName} является предком класса {type2.FullName}";
 
-      throw new ArgumentException("Не удалось определить степень родства между классами");
+      throw new ArgumentException($"Не удалось определить степень родства между классами {obj1.GetType().FullName} and {obj1.GetType().FullName}");
     }
-  }
 
+    static string GetCommonAncestors(object obj1, object obj2)
+    {
+      var ancestorsType1 = GetAncestors(obj1.GetType());
+      var ancestorsType2 = GetAncestors(obj2.GetType());
+
+      var commonAncestor = ancestorsType1
+        .Intersect(ancestorsType2)
+        .Where(x => x != typeof(object));
+
+      if (commonAncestor.Any())
+      {
+        return string.Join(",", commonAncestor.Select(x => x.FullName));
+      }
+
+      return string.Empty;
+    }
+
+    static IEnumerable<Type> GetAncestors(Type type)
+    {
+      var baseType = type.BaseType;
+      while (baseType != null)
+      {
+        yield return baseType;
+        baseType = baseType.BaseType;
+      }
+    }
+
+  }
 
   class Entity
   {
